f8c77905b2d5fb68984f73ab8fccb8d30417e840
Craig Weber <crgwbr@gmail.com>
2016-06-27 22:41:40 -0400

    Fix bug saving user in dashboard app view

--- wellsfargo/dashboard/views.py
+++ wellsfargo/dashboard/views.py
@@ -87,7 +87,7 @@ class CreditApplicationView(FormView):
             try:
                 app = form.save(commit=False)
                 resp = actions.submit_credit_application(app)
-                account = resp.save()
+                account = resp.save(app.user)
                 return self.form_valid(account)
             except CreditApplicationDenied as e:
                 messages.add_message(request, messages.ERROR, _('Credit Application was denied by Wells Fargo'))
